e2f0267471ba9aaea38c8a5bbcec676a3fa952c8
Emre Soykan <120596160+soykannemree@users.noreply.github.com>
2023-09-01 19:05:50 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -11,25 +11,41 @@ var result = 0;
 show(result);
 
 
-//which button clicked
+
+// this part defines which button clicked 
+
+// the calculator takes the number as a string parameter
+
+// user clicked any 4 mathematical processes signs 
+// than stops taking parameter fom user
+
 $( ".button" ).on( "click", function() {
+    
     let clickedButton = $(this).html();
-    console.log(clickedButton)
+    
+    console.log(clickedButton);
 
     if (clickedButton ==="AC") {
+        
         result = 0;
         inputNumber = '0';
+        
     }
     else if (clickedButton==='+' || clickedButton==='-' || clickedButton==='x' || clickedButton==='/') {
+        
         num1 = parseInt(inputNumber);
         operator = clickedButton;
         inputNumber = '';
+        
     }
     else if(clickedButton === '='){
+        
         inputNumber = transaction(num1,inputNumber,operator);
         operator = null;
+        
     }
     else{
+        
         if (inputNumber === '0'){
             inputNumber = clickedButton;
         } 
@@ -37,30 +53,39 @@ $( ".button" ).on( "click", function() {
             inputNumber = inputNumber + clickedButton;
         } 
     }
-
     show(inputNumber);
 
 });
 
+
 function show(results) {
+    
     var results = results.toString();
     $("h1").html(results);
+    
 }
   
 
 //All processes
 function transaction(num1,num2,operator) {
+    
     num1 = parseInt(num1);
     num2 =parseInt(num2);
+    
     switch (operator) {
+            
         case '/':
-            return num1/num2;         
+            return num1/num2;
+            
          case 'x':
-            return num1*num2;            
+            return num1*num2; 
+            
         case '-':
-            return num1-num2;      
+            return num1-num2;  
+            
         case '+':
-            return num1+num2;            
+            return num1+num2;  
+            
         default:
             break;
     }
